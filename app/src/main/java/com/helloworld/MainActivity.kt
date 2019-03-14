af0266eb8c349d1d49b0e2eaba02c811a1775799
Tahir <kiplmobile11@konstantinfosolutions.com>
2019-03-14 18:57:54 +0530

    This is test commit on master

--- app/src/main/java/com/helloworld/MainActivity.kt
+++ app/src/main/java/com/helloworld/MainActivity.kt
@@ -2,11 +2,13 @@ package com.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("Test", "This is test commit on master")
     }
 }
