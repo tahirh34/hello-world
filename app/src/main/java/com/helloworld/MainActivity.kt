08bbb7e8294f910bcfe98d37d50409ba73518786
Tahir <kiplmobile11@konstantinfosolutions.com>
2019-03-14 19:41:45 +0530

    First commit on test feature branch

--- app/src/main/java/com/helloworld/MainActivity.kt
+++ app/src/main/java/com/helloworld/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Test", "My develop")
+        Log.e("Test", "This is test feature branch")
     }
 }
