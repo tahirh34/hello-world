f4c441271351ceabe48593ddbcbf52a000175834
Tahir <kiplmobile11@konstantinfosolutions.com>
2019-03-14 19:39:18 +0530

    My develop

--- app/src/main/java/com/helloworld/MainActivity.kt
+++ app/src/main/java/com/helloworld/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Test", "Test merge develop to master branch and finish develop branch")
+        Log.e("Test", "My develop")
     }
 }
