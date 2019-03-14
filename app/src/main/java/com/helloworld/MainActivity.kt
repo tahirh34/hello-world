5496eb8c744b38f79ae7a9acf3f8cc2e99a022a6
Tahir <kiplmobile11@konstantinfosolutions.com>
2019-03-14 19:35:21 +0530

    Test commit

--- app/src/main/java/com/helloworld/MainActivity.kt
+++ app/src/main/java/com/helloworld/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Test", "This is test commit on master")
+        Log.e("Test", "Test")
     }
 }
